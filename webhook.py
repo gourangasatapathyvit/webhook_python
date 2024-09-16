18a610935c67714856d1787a14bccf32ac5b818c
Gouranga satapathy <Pulusatapathy@gmail.com>
2024-09-16 13:18:19 +0530

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 webhook_url = 'http://127.0.0.1:5000/webhook'
-# upi merchandise like PHONEpay (CLIENT side)
+# BANK side like SBI,HDFC bank OR razorpay (it act like server side, which will return response to webhook.py)
 data = { 'name': 'ssddds Journey', 
          'Channel URL': 'https://www.youtube.com/channel/UC4Snw5yrSDMXys31I18U3gg' }
 
